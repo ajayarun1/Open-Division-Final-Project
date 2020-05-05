8a756dd823a504cfde3c1abab3c18ef9fe986f6c
ajayarun1 <ajayka2@illinois.edu>
2020-05-05 03:44:43 -0500

    2 screens done! (GIF generator function added too)

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 //import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Linking, FlatList, TextInput } from 'react-native';
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Input, Button } from 'react-native-elements';
@@ -9,7 +10,6 @@ import Constants from 'expo-constants';
 import { Audio, Video } from 'expo-av';
 import Hyperlink from 'react-native-hyperlink'
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Linking } from 'react-native'
 
 const playbackObject = new Audio.Sound();
 const meditationWalthrough1 = new Audio.Sound();
@@ -26,9 +26,6 @@ function pauseResume() {
     meditationWalthrough1.playAsync()
   }
 }
-
-//NOTE TO SELF (5/2): add another mp3 file for mediation walkthrough in the meditation screen, leave no audio in help from others, and add motivational music in moving forward screen
-// After completing audio, work on adding text and info to the three screens
 function HomeScreen({ navigation }) {
   //Serene music plays to battle depressed feelings, different audio for each screen
   (async () => {
@@ -138,7 +135,7 @@ function meditationScreen({ navigation }) {
   })();
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ color: '#D4AF37', fontWeight:'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+      <Text style={{ color: '#D4AF37', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
         Welcome to the Zen Room! Currently playing is a mediation walkthrough from a Quarantine-Zone approved field expert. Please click the butons below to play/resume the audio, or go back to the home screen!
         </Text>
       <Button
@@ -160,10 +157,10 @@ function meditationScreen({ navigation }) {
       />
       <Separator />
       <Hyperlink linkDefault={true}>
-        <Text style={{ fontSize: 15, textAlign: 'center', color: '#D4AF37', fontWeight:'bold' }}>
+        <Text style={{ fontSize: 15, textAlign: 'center', color: '#D4AF37', fontWeight: 'bold' }}>
           Please close your eyes and follow along with today's digital yoga instructor! When finished, click below for today's yoga exercise!
     </Text>
-    <Text>{'\n'}</Text>
+        <Text>{'\n'}</Text>
         <Text style={{ color: 'blue', fontSize: 13, fontWeight: 'bold', textAlign: 'center' }}>
           https://www.youtube.com/watch?v=d4S4twjeWTs
     </Text>
@@ -190,17 +187,273 @@ function meditationScreen({ navigation }) {
   );
 }
 function spiritsScreen({ navigation }) {
+  var randNum = Math.floor(Math.random() * 13);
   (async () => {
     await
       await playbackObject.stopAsync()
   })();
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text> Lift your spirits!</Text>
-      <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
-    </View>
-  );
+  if (randNum == 0) {
+    //Walk in and make you smile
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 250, width: 550 }}
+          source={{
+            uri: 'https://media.giphy.com/media/3o7WTArz1fjYVGbNU4/giphy.gif'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
+  if (randNum == 1) {
+    //Arial pizza
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 270, width: 430 }}
+          source={{
+            uri: 'https://66.media.tumblr.com/f2ffb6cf43c384ba3081d635c4a76eeb/tumblr_n4r2213bOI1s5e5bko1_500.gifv'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
+  if (randNum == 2) {
+    //Kid getting a face full of mail
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 475, width: 300 }}
+          source={{
+            uri: 'https://i.chzbgr.com/full/7387151872/hABCABA7F/kid-gets-a-face-full-of-mail'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
+  if (randNum == 3) {
+    //Dog in palm of hand
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 300, width: 300 }}
+          source={{
+            uri: 'https://33.media.tumblr.com/c486b1b1f635af074d9be850ce70bba1/tumblr_nlotnf4lnw1u17yx1o1_500.gif'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
+  if (randNum == 4) {
+    //Random hug
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 300, width: 300 }}
+          source={{
+            uri: 'https://i.imgur.com/XB2Rmnt.gif'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
+  if (randNum == 5) {
+    //Llamas
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 300, width: 300 }}
+          source={{
+            uri: 'https://cdn.shopify.com/s/files/1/2353/7225/t/96/assets/funny-llama-alpaca-pictures-hilarious-make-you-smile_8207.jpg'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
+  if (randNum == 6) {
+    //Cat jumping out of pepsi box
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 300, width: 400 }}
+          source={{
+            uri: 'https://www.laughtard.com/wp-content/uploads/2017/05/Drink.gif'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
+  if (randNum == 7) {
+    //Cows
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 300, width: 400 }}
+          source={{
+            uri: 'https://2.bp.blogspot.com/-oowyWDWhavE/Wdz34-Co6WI/AAAAAAAAUMY/dYMHeu5SIb48yPDuV7Ou_o7ejAl42zV0gCLcBGAs/s1600/Cattle%2BCalves%2BGif.gif'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
+  if (randNum == 8) {
+    //Cat with toy
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 300, width: 400 }}
+          source={{
+            uri: 'https://www.awesomeinventions.com/wp-content/uploads/2015/11/cat-toy.gif'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
+  if (randNum == 9) {
+    //Smiling at your crush
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 300, width: 400 }}
+          source={{
+            uri: 'https://i0.wp.com/livelypals.com/wordpress/wp-content/uploads/2019/03/22458254aca4a1cfa9a21cfa5c410fa7.gif?fit=360%2C308&ssl=1'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
+  if (randNum == 10) {
+    //Old man in onion ring
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 410, width: 350 }}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR4tMhjfGHp5jZ7mhWBL-9AgJ_35y-ZlxGPHgQrinwJD-fhSHps&usqp=CAU'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
+  if (randNum == 11) {
+    //Cat with paws stretched out
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 300, width: 400 }}
+          source={{
+            uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2017-06/23/11/asset/buzzfeed-prod-fastlane-02/anigif_sub-buzz-24511-1498231293-3.gif?downsize=600:*&output-format=auto&output-quality=auto'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
+  if (randNum == 12) {
+    //Elephant
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FF69B4', fontWeight: 'bold', textAlign: 'center', fontSize: 18, lineHeight: 25, height: 125, margin: 15 }}>
+          Need a quick little pick-me-up? Something to lighten up your mood, put a smile on your face? Well, you're in the right place!
+      </Text>
+        <Image
+          style={{ height: 300, width: 400 }}
+          source={{
+            uri: 'https://4.bp.blogspot.com/-ZzU8ZYgjlLo/UEogi29n9rI/AAAAAAAAAGM/9G4yuf_DHw0/s320/funny-quotes-about-friends_6428_1.gif'
+          }}
+        />
+        <Separator />
+        <Button title="Another one!" onPress={() => { (navigation.navigate('Home')); (navigation.navigate('spirits')); }} />
+        <Separator />
+        <Button title="Go to Home" onPress={() => { (navigation.navigate('Home')); playbackObject.playAsync() }} />
+      </View>
+    );
+  }
 }
+
 function othersScreen({ navigation }) {
   (async () => {
     await
